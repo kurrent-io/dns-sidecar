fcabf19c6c6d155a722ffaa74f1d38e32b350be8
Ryan B <197610600+rb-kurrent@users.noreply.github.com>
2025-10-27 05:18:34 -0600

    build a redhat image too

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
+ARG BASE=gcr.io/distroless/static:nonroot
 # Copy a pre-cross-compiled binary into an alpine container.
 # Use alpine because we can get setcap in the container, so macos can build the images.
 FROM alpine AS build
@@ -8,8 +9,14 @@ COPY $TARGETOS/$TARGETARCH /dns
 RUN setcap 'cap_net_bind_service+ep' /dns
 
 # Then copy the setcap'd binary into our final dockerfile
-FROM gcr.io/distroless/static:nonroot
+FROM $BASE
 WORKDIR /
 USER 65532:65532
 ENTRYPOINT ["/dns"]
+LABEL name=dns \
+      maintainer="Kurrent, Inc" \
+      vendor="Kurrent, Inc" \
+      summary="DNS sidecar image for KurrentDB pods" \
+      description="Serves Split-DNS as configured by KurrentDB Operator and KurrentDB resource."
+COPY licenses /licenses
 COPY --from=build /dns /dns
--- build.sh
+++ build.sh
@@ -57,24 +57,47 @@ fi
 if [ -n "$want_cross_compile" ] ; then
     show GOOS=linux GOARCH=amd64 compile build/docker/linux/amd64
     show GOOS=linux GOARCH=arm64 compile build/docker/linux/arm64
+    mkdir -p build/docker/licenses
+    cp LICENSE.txt build/docker/licenses
 fi
 
+dynamic_labels () {
+    echo --label release="$(git show-ref HEAD --head -s8)" --label version=$VERSION
+}
+
 if [ -n "$want_image" ] ; then
-    show docker build -t $IMAGE -f Dockerfile build/docker
+    show docker build -f Dockerfile $(dynamic_labels) build/docker -t $IMAGE
+    show docker build -f Dockerfile $(dynamic_labels) build/docker -t $IMAGE-rhel8 \
+        --build-arg BASE="registry.access.redhat.com/ubi8/ubi-micro"
 fi
 
 publish () {
     repo="$1"
     # replace download-name/kurrent-latest with upload-name/$repo
     image="docker.cloudsmith.io/eventstore/$repo/${IMAGE#docker.kurrent.io/kurrent-latest/}"
+
+    # publish standard image
     show docker buildx build \
         --push \
         --provenance=false \
         --sbom=false \
         --platform=linux/arm64/v8,linux/amd64 \
-        -t=$image \
+        $(dynamic_labels) \
         -f Dockerfile \
-        build/docker
+        build/docker \
+        -t $image
+
+    # publish redhat image
+    show docker buildx build \
+        --push \
+        --provenance=false \
+        --sbom=false \
+        --platform=linux/arm64/v8,linux/amd64 \
+        $(dynamic_labels) \
+        -f Dockerfile \
+        build/docker \
+        -t $image-rhel8 \
+        --build-arg BASE="registry.access.redhat.com/ubi8/ubi-micro"
 }
 
 if [ -n "$want_publish_staging" ] ; then
